c2c30bcf294907e124c2d1029a540ed715b056ea
Gambya <robertolopesramos@gmail.com>
2015-03-16 23:59:41 -0300

    Add MapaActivity no projeto.

---
+++ PubDay/src/com/mindtrip/pubday/MapaActivity.java
@@ -0,0 +1,39 @@
+package com.mindtrip.pubday;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+public class MapaActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mapa);
+		
+		((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.mapa, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- PubDay/src/com/mindtrip/pubday/PubDayActivity.java
+++ PubDay/src/com/mindtrip/pubday/PubDayActivity.java
@@ -12,6 +12,7 @@ public class PubDayActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pub_day);
+        
     }
 
 
